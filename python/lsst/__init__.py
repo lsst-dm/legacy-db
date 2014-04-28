e19a7e2f4a5bfbe65d27ea03531d292c12df0533
Jacek Becla <becla@slac.stanford.edu>
2014-04-28 15:01:02 -0700

    conditional import for non-eups builds

--- python/lsst/__init__.py
+++ python/lsst/__init__.py
@@ -1,2 +1,5 @@
-import pkgutil, lsstimport
-__path__ = pkgutil.extend_path(__path__, __name__)
+try:
+    import pkgutil, lsstimport
+    __path__ = pkgutil.extend_path(__path__, __name__)
+except:
+    pass
